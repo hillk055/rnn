4204f9243c2c471efc400b75f9bbb6cc269bf0e0
hillk055 <101193869+hillk055@users.noreply.github.com>
2025-10-21 23:52:26 +0100

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -1,20 +1,7 @@
 from transformers import AutoTokenizer
 from pathlib import Path
 import torch.nn as nn
-
-text = Path('corpus.txt').read_text(encoding='utf-8')   # Encoding in utf-8 is standard practice
-
-tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-encoded = tokenizer(text, return_tensors='pt', truncation=True, add_special_tokens=True, return_overflowing_tokens=True,
-                    stride=32, max_length=96, padding='max_length')
-# Set truncation True and Max Length to limit the sie of the text
-# Special tokens are either sentence beginners or sentences breakers, marked in so the model has extra context
-
-# Next we need to batch inputs, we need some overlap so that the model can learn conditionality.
-# E.g. I liked up at the sky. The stars looked good. The model now knows that sky and starts can be used together
-
-print(encoded['input_ids'].shape) # Returns number of batches and the size of each batch
-vocab_sz = tokenizer.vocab_size
+import torch
 
 
 class RNNLanguageModel(nn.Module):
@@ -29,11 +16,32 @@ class RNNLanguageModel(nn.Module):
         # sequence with a word in the vocabulary
         self.fc = nn.Linear(10, vocab_size)
 
-
     def forward(self, x):
 
-        pass
+        out = self.embed(x)
+        out, hidden = self.rnn(out)
+        out = self.fc(out)
+        return out
 
+
+text = Path('corpus.txt').read_text(encoding='utf-8')   # Encoding in utf-8 is standard practice
+
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+encoded = tokenizer(text, return_tensors='pt', truncation=True, add_special_tokens=True, return_overflowing_tokens=True,
+                    stride=32, max_length=96, padding='max_length')
+# Set truncation True and Max Length to limit the sie of the text
+# Special tokens are either sentence beginners or sentences breakers, marked in so the model has extra context
+
+# Next we need to batch inputs, we need some overlap so that the model can learn conditionality.
+# E.g. I liked up at the sky. The stars looked good. The model now knows that sky and starts can be used together
+
+print(encoded['input_ids'].shape)   # Returns number of batches and the size of each batch
+vocab_sz = tokenizer.vocab_size
+
+# Set up the model, criterion and optimiser for the model
+model = RNNLanguageModel(vocab_size=vocab_sz)
+criterion = nn.CrossEntropyLoss()
+optimiser = torch.optim.ADAM(model.parameters(), lr=0.001)
 
 
 
